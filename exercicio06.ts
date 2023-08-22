96e66044d52969266f9678b6b1c2192bdd17444e
Linguanotto <46543254+linguanotto@users.noreply.github.com>
2023-08-21 21:21:22 -0300

    Add files via upload

---
+++ exercicio06.ts
@@ -0,0 +1,8 @@
+  
+
+    (function () {
+        var numero1 = 2;
+        var numero2 = 3;
+        var resultado = numero1 + numero2;
+        console.log('A soma de ' + numero1 + ' e ' + numero2 + ' é igual a ' + resultado);
+    })();
